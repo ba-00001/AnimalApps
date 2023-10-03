51fa6b08988f37c77caa050ac046633c0a0e583b
ba-00001 <124537281+ba-00001@users.noreply.github.com>
2023-10-03 01:55:33 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/myownapp_and101/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.myownapp_and101
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    //Button myButton = findViewById(R.id.myButton);
+    //myButton.setTooltipText("This is a tooltip text");
+}
